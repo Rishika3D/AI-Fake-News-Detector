47d28f7d9873ac08943666108169702618baea4e
Rishika Kherniwal <rishikakherniwal0809@gmail.com>
2026-03-02 00:39:16 +0530

    Improved backend

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: 5432,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT || 5432,
 });
 
 db.connect()
